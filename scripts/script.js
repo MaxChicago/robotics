3adcd36b7a2e7a3cf74c57cec6600170deae03b7
MaxChicago <104435795+MaxChicago@users.noreply.github.com>
2025-01-20 16:32:00 +0300

    Add files via upload
    Signed-off-by: MaxChicago <104435795+MaxChicago@users.noreply.github.com>

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+$('.slider-wrapper').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1100,
+  dots: true,
+  cssEase: 'linear',
+
+})
+
+$('.slider-wrapper').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1100,
+  dots: true,
+  cssEase: 'linear',
+
+})
